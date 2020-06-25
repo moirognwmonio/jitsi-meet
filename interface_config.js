d76b82efdbf984dcb1b4f77209018a3272e281e3
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-06-25 15:58:04 -0300

    [IMPROVEMENT] Add Rocket.Chat App schema

--- interface_config.js
+++ interface_config.js
@@ -245,12 +245,12 @@ var interfaceConfig = {
     /**
      * Specify mobile app scheme for opening the app from the mobile browser.
      */
-    APP_SCHEME: 'org.jitsi.meet',
+    APP_SCHEME: 'rocketchat',
 
     /**
      * Specify the Android app package name.
      */
-    ANDROID_APP_PACKAGE: 'org.jitsi.meet',
+    ANDROID_APP_PACKAGE: 'chat.rocket.android',
 
     /**
      * Override the behavior of some notifications to remain displayed until
